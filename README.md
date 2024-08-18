04872a506ed918bd928f9a95de6934658313bb6a
ezacarias <ezacarias@izzi.mx>
2024-08-18 14:56:44 -0600

    263.Creando el Formulario de Login

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ## 260.¿Que es Vuetify? e instalación
 ## 261.Componentes en Vuetify
 ## 262.Creando la vista de Login
+## 263.Creando el Formulario de Login
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,4 +2,22 @@
 </script>
 <template>
     <h1 class="text-center text-h3 font-weight-bold my-5">iniciar Sesión</h1>
+    <v-card flat max-width="600" class="mx-auto my-10">
+        <v-car-tittle
+        class="text-h4 font-weight-bold"
+        tag="h3">
+            Iniciar Sesión
+        </v-car-tittle>
+        <v-car-subtitle class="text-h5">
+            Inicia Sesión con tu cuenta
+        </v-car-subtitle>
+        <v-form class="mt-5">
+            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5"/>
+
+            <v-text-field type="password" label="password" bg-color="blue-grey-lighten-5"/>
+        </v-form>
+        <v-btn block color="pink-accent-3">
+            Iniciar Sesión
+        </v-btn>
+    </v-card>
 </template>
