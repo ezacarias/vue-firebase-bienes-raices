7e3720bfbbfd0e06456fa54cb2db0256fc444bf9
ezacarias <ezacarias@izzi.mx>
2024-08-18 15:01:34 -0600

    265.- Mostrando los mensajes de error en la validación

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 ## 262.Creando la vista de Login
 ## 263.Creando el Formulario de Login
 ## 264 Validación con Vee-validate
+## 265.- Mostrando los mensajes de error en la validación
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,31 +1,42 @@
 <script setup>
-import { useForm, useField} from 'vee-validate'
+import { useForm, useField } from "vee-validate";
+import { loginSchema as validationSchema } from "../validation/loginSchema";
 
-const {handleSubmit}=useForm()
+const { handleSubmit } = useForm({ validationSchema });
 
-const email =useField('email')
-const password =useField('password')
+const email = useField("email");
+const password = useField("password");
 
-
+const submit = handleSubmit(() => {});
 </script>
 <template>
-    <h1 class="text-center text-h3 font-weight-bold my-5">iniciar Sesión</h1>
-    <v-card flat max-width="600" class="mx-auto my-10">
-        <v-car-tittle
-        class="text-h4 font-weight-bold"
-        tag="h3">
-            Iniciar Sesión
-        </v-car-tittle>
-        <v-car-subtitle class="text-h5">
-            Inicia Sesión con tu cuenta
-        </v-car-subtitle>
-        <v-form class="mt-5">
-            <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" v-model="email.value.value"/>
+  <h1 class="text-center text-h3 font-weight-bold my-5">iniciar Sesión</h1>
+  <v-card flat max-width="600" class="mx-auto my-10">
+    <v-car-tittle class="text-h4 font-weight-bold" tag="h3">
+      Iniciar Sesión
+    </v-car-tittle>
+    <v-car-subtitle class="text-h5">
+      Inicia Sesión con tu cuenta
+    </v-car-subtitle>
+    <v-form class="mt-5">
+      <v-text-field
+        type="email"
+        label="Email"
+        class="mb-5"
+        bg-color="blue-grey-lighten-5"
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
+      />
 
-            <v-text-field type="password" label="password" bg-color="blue-grey-lighten-5" v-model="password.value.value"/>
-        </v-form>
-        <v-btn block color="pink-accent-3">
-            Iniciar Sesión
-        </v-btn>
-    </v-card>
+      <v-text-field
+        type="password"
+        label="password"
+         class="mb-5"
+        bg-color="blue-grey-lighten-5"
+        v-model="password.value.value"
+        :error-messages="password.errorMessage.value"
+      />
+    </v-form>
+    <v-btn block color="pink-accent-3" @click="submit"> Iniciar Sesión </v-btn>
+  </v-card>
 </template>
