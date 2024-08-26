6423dddac4d74c6b0d0db7a8a289950a8a78d152
ezacarias <ezacarias@izzi.mx>
2024-08-25 21:53:01 -0600

    269.- Creando un diccionario de errores

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -3,14 +3,20 @@ import{useFirebaseAuth} from 'vuefire'
 import {signInWithEmailAndPassword} from 'firebase/auth'
 
 export const useAuthStore  = defiineStore('Auth', () =>{
+
     const auth = useFirebaseAuth()
+
+    const errorCodes ={
+        'auth/user-not-found' :'Usuario no encontrado',
+        'auth/wrong-password' : 'El password es incorrecto'
+    }
     const login = (email, password) =>{
         signInWithEmailAndPassword(auth,email,password)
         .then((userCredential)=>{
 
         })
         .catch(error=>{
-            
+            console.log(errorCodes[error.code])
         })
     }
     return{
